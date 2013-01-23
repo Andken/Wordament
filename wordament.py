bd680cb574eedd6843e6e8541b836910d7c5db3a
Ben Andken <ben.andken@gmail.com>
2013-01-22 20:53:49 -0500

    parser beginnings

---
+++ wordament.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+def inBox(w, b):
+    return true
+
+
+y1 = raw_input("?: ")
+y2 = raw_input("?: ")
+y3 = raw_input("?: ")
+y4 = raw_input("?: ")
+
+
+#wordlist = open("WORD_3_LETTERS.LST", "r").readlines()
+#
+#for w in wordlist:
+#    if inBox(w, b):
+#        print w
+#
+#wordlist.close()
+
+
+print y1
+print y2
+print y3
+print y4
+
