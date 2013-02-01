7d2732d48a58df7da40c65fa3acddbd821c81bac
Ben Andken <ben@andken.com>
2013-02-01 15:05:18 -0500

    better test graph

--- test.py
+++ test.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
 
-
-
-
-
 import wordament
 
-test = "111111111111quit"
+test = "queedoorkoobenac"
 
 print test[0:4]
 print test[4:8]
 print test[8:12]
 print test[12:16]
 
+
 result = wordament.solve(test)
 
 for r in result:
     tweet = "%s - %s" % (r[0], r[1])
     print "   ", tweet, r[2]
+
+print test[0:4]
+print test[4:8]
+print test[8:12]
+print test[12:16]
+
