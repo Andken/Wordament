537990d88a70634d5c793af81fdfd889e09aac8d
Ben Andken <ben@andken.com>
2013-02-18 18:56:59 -0500

    forgot settings file

---
+++ nxt/settings.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import math
+
+# robot geometry
+# The units are Lego units
+E = 12.0;      # end effector
+F = 46.0;      # base
+RE = 22.0;
+RF = 8.0;
+ 
+# trigonometric constants
+SQRT3 = math.sqrt(3.0);
+PI = 3.141592653;    # PI
+SIN120 = SQRT3/2.0;   
+COS120 = -0.5;        
+TAN60 = SQRT3;
+SIN30 = 0.5;
+TAN30 = 1/SQRT3;
+ 
