4e7bfa17238add44fd42fd59b443606990b9666c
Ben Andken <ben@andken.com>
2013-02-18 19:28:01 -0500

    DeltaRobot.py seems to work better

--- nxt/DeltaRobot.py
+++ nxt/DeltaRobot.py
@@ -1,103 +1,101 @@
 #!/usr/bin/python
 
-import sin from math
-import cos from math
-import tan from math
-import atan from math
-import sqrt from math
-import pi from math
+from math import pi
+from math import sin
+from math import cos
+from math import tan
+from math import atan
+from math import sqrt
 
 # adapted from: http://www.rjmcnamara.com/wp-content/uploads/2010/12/DeltaRobot/DeltaRobot.C
 
 # robot geometry
 # The units are Lego units
-e = 12.0;     # end effector
-f = 46.0;     # base
-re = 22.0;
-rf = 8.0;
+e = 12.0     # end effector
+f = 46.0     # base
+re = 22.0
+rf = 8.0
  
- // trigonometric constants
- const float sqrt3 = sqrt(3.0);
- const float pi = 3.141592653;    // PI
- const float sin120 = sqrt3/2.0;   
- const float cos120 = -0.5;        
- const float tan60 = sqrt3;
- const float sin30 = 0.5;
- const float tan30 = 1/sqrt3;
+# trigonometric constants
+# const float sqrt3 = sqrt(3.0)
+# const float pi = 3.141592653    # PI
+# const float sin120 = sqrt3/2.0   
+# const float cos120 = -0.5        
+# const float tan60 = sqrt3
+# const float sin30 = 0.5
+# const float tan30 = 1/sqrt3
  
- // forward kinematics: (theta1, theta2, theta3) -> (x0, y0, z0)
- // returned status: 0=OK, -1=non-existing position
- int delta_calcForward(float theta1, float theta2, float theta3, float &x0, float &y0, float &z0) {
-     float t = (f-e)*tan30/2;
-     float dtr = pi/(float)180.0;
+# forward kinematics: (theta1, theta2, theta3) -> (x0, y0, z0)
+def delta_calcForward(theta1, theta2, theta3):
+    t = (f-e)*tan(pi/6)/2
  
-     theta1 *= dtr;
-     theta2 *= dtr;
-     theta3 *= dtr;
- 
-     float y1 = -(t + rf*cos(theta1));
-     float z1 = -rf*sin(theta1);
- 
-     float y2 = (t + rf*cos(theta2))*sin30;
-     float x2 = y2*tan60;
-     float z2 = -rf*sin(theta2);
- 
-     float y3 = (t + rf*cos(theta3))*sin30;
-     float x3 = -y3*tan60;
-     float z3 = -rf*sin(theta3);
- 
-     float dnm = (y2-y1)*x3-(y3-y1)*x2;
- 
-     float w1 = y1*y1 + z1*z1;
-     float w2 = x2*x2 + y2*y2 + z2*z2;
-     float w3 = x3*x3 + y3*y3 + z3*z3;
+    y1 = -(t + rf*cos(theta1))
+    z1 = -rf*sin(theta1)
+    
+    y2 = (t + rf*cos(theta2))*sin(pi/6)
+    x2 = y2*tan(pi/3)
+    z2 = -rf*sin(theta2)
+
+    y3 = (t + rf*cos(theta3))*sin(pi/6)
+    x3 = -y3*tan(pi/3)
+    z3 = -rf*sin(theta3)
+
+    dnm = (y2-y1)*x3-(y3-y1)*x2
+    
+    w1 = y1*y1 + z1*z1
+    w2 = x2*x2 + y2*y2 + z2*z2
+    w3 = x3*x3 + y3*y3 + z3*z3
      
-     // x = (a1*z + b1)/dnm
-     float a1 = (z2-z1)*(y3-y1)-(z3-z1)*(y2-y1);
-     float b1 = -((w2-w1)*(y3-y1)-(w3-w1)*(y2-y1))/2.0;
+    # x = (a1*z + b1)/dnm
+    a1 = (z2-z1)*(y3-y1)-(z3-z1)*(y2-y1)
+    b1 = -((w2-w1)*(y3-y1)-(w3-w1)*(y2-y1))/2.0
  
-     // y = (a2*z + b2)/dnm;
-     float a2 = -(z2-z1)*x3+(z3-z1)*x2;
-     float b2 = ((w2-w1)*x3 - (w3-w1)*x2)/2.0;
+    # y = (a2*z + b2)/dnm
+    a2 = -(z2-z1)*x3+(z3-z1)*x2
+    b2 = ((w2-w1)*x3 - (w3-w1)*x2)/2.0
  
-     // a*z^2 + b*z + c = 0
-     float a = a1*a1 + a2*a2 + dnm*dnm;
-     float b = 2*(a1*b1 + a2*(b2-y1*dnm) - z1*dnm*dnm);
-     float c = (b2-y1*dnm)*(b2-y1*dnm) + b1*b1 + dnm*dnm*(z1*z1 - re*re);
+    # a*z^2 + b*z + c = 0
+    a = a1*a1 + a2*a2 + dnm*dnm
+    b = 2*(a1*b1 + a2*(b2-y1*dnm) - z1*dnm*dnm)
+    c = (b2-y1*dnm)*(b2-y1*dnm) + b1*b1 + dnm*dnm*(z1*z1 - re*re)
   
-     // discriminant
-     float d = b*b - (float)4.0*a*c;
-     if (d < 0) return -1; // non-existing point
+    # discriminant
+    d = b*b - 4.0*a*c
+    if (d < 0):
+        # non-existing point
+        sys.exit(1)
  
-     z0 = -(float)0.5*(b+sqrt(d))/a;
-     x0 = (a1*z0 + b1)/dnm;
-     y0 = (a2*z0 + b2)/dnm;
-     return 0;
- }
+    z0 = -0.5*(b+sqrt(d))/a
+    x0 = (a1*z0 + b1)/dnm
+    y0 = (a2*z0 + b2)/dnm
+    return theta1, theta2, theta3
  
- // inverse kinematics
- // helper functions, calculates angle theta1 (for YZ-pane)
- int delta_calcAngleYZ(float x0, float y0, float z0, float &theta) {
-     float y1 = -0.5 * 0.57735 * f; // f/2 * tg 30
-     y0 -= 0.5 * 0.57735    * e;    // shift center to edge
-     // z = a + b*y
-     float a = (x0*x0 + y0*y0 + z0*z0 +rf*rf - re*re - y1*y1)/(2*z0);
-     float b = (y1-y0)/z0;
-     // discriminant
-     float d = -(a+b*y1)*(a+b*y1)+rf*(b*b*rf+rf); 
-     if (d < 0) return -1; // non-existing point
-     float yj = (y1 - a*b - sqrt(d))/(b*b + 1); // choosing outer point
-     float zj = a + b*yj;
-     theta = 180.0*atan(-zj/(y1 - yj))/pi + ((yj>y1)?180.0:0.0);
-     return 0;
- }
  
- // inverse kinematics: (x0, y0, z0) -> (theta1, theta2, theta3)
- // returned status: 0=OK, -1=non-existing position
- int delta_calcInverse(float x0, float y0, float z0, float &theta1, float &theta2, float &theta3) {
-     theta1 = theta2 = theta3 = 0;
-     int status = delta_calcAngleYZ(x0, y0, z0, theta1);
-     if (status == 0) status = delta_calcAngleYZ(x0*cos120 + y0*sin120, y0*cos120-x0*sin120, z0, theta2);  // rotate coords to +120 deg
-     if (status == 0) status = delta_calcAngleYZ(x0*cos120 - y0*sin120, y0*cos120+x0*sin120, z0, theta3);  // rotate coords to -120 deg
-     return status;
- }
+# inverse kinematics
+# helper functions, calculates angle theta1 (for YZ-pane)
+def delta_calcAngleYZ(x0, y0, z0):
+    y1 = -0.5 * 0.57735 * f # f/2 * tg 30
+    y0 -= 0.5 * 0.57735    * e    # shift center to edge
+    # z = a + b*y
+    a = (x0*x0 + y0*y0 + z0*z0 +rf*rf - re*re - y1*y1)/(2*z0)
+    b = (y1-y0)/z0
+    # discriminant
+    d = -(a+b*y1)*(a+b*y1)+rf*(b*b*rf+rf) 
+    if (d < 0):
+        # non-existing point
+        sys.exit(2)
+
+    yj = (y1 - a*b - sqrt(d))/(b*b + 1) # choosing outer point
+    zj = a + b*yj
+    theta = atan(-zj/(y1 - yj)) + pi if(yj>y1) else 0.0
+    return theta
+ 
+ 
+# inverse kinematics: (x0, y0, z0) -> (theta1, theta2, theta3)
+# returned status: 0=OK, -1=non-existing position
+def delta_calcInverse(x0, y0, z0):
+    theta1 = delta_calcAngleYZ(x0, y0, z0)
+    theta2 = delta_calcAngleYZ(x0*cos(2*pi/3) + y0*sin(2*pi/3), y0*cos(2*pi/3)-x0*sin(2*pi/3), z0)  # rotate coords to +120 deg
+    theta3 = delta_calcAngleYZ(x0*cos(2*pi/3) - y0*sin(2*pi/3), y0*cos(2*pi/3)+x0*sin(2*pi/3), z0)  # rotate coords to -120 deg
+    return theta1, theta2, theta3
+
--- nxt/test.py
+++ nxt/test.py
@@ -5,9 +5,9 @@ from DeltaRobot import *
 print "test start"
 
 x, y, z = delta_calcForward(0.0,0.0,0.0)
-t1, t2, t3 = delta_calcInverse(x, y, z)
-x2, y2, z2 = delta_calcForward(t1, t2, t3)
+#t1, t2, t3 = delta_calcInverse(x, y, z)
+#x2, y2, z2 = delta_calcForward(t1, t2, t3)
 print x, y, z
-print t1, t2, t3
-print x2, y2, z2
+#print t1, t2, t3
+#print x2, y2, z2
 
