98f35d2f73cd2935b8e42adce21f60406dff7e96
Ben Andken <ben@andken.com>
2013-03-06 08:23:32 -0500

    alll motors moving at the same time, with proper feedback for each motor

--- nxt/working.py
+++ nxt/working.py
@@ -23,39 +23,51 @@ working_power = 100
 b = nxt.locator.find_one_brick()
 m = MotCont(b)
 m.start()
-m1 = Motor(b, PORT_A)
+ma = Motor(b, PORT_A)
+mb = Motor(b, PORT_B)
+mc = Motor(b, PORT_C)
 
 while 1:
-    pos1 = 45
+    pos1a = 90
+    pos1b = 45
+    pos1c = 10
     pos2 = 0
 
     time.sleep(1)
-    print "Before move: ", m1.get_tacho().rotation_count
+    print "Before move: ", ma.get_tacho().rotation_count
     
-    new_pos = abs(m1.get_tacho().rotation_count - pos1)
-    power = -1*int(copysign(working_power,m1.get_tacho().rotation_count - pos1))
-    runinstruction(PORT_A, power, new_pos);
-    runinstruction(PORT_B, power, new_pos);
-    runinstruction(PORT_C, power, new_pos);
+    new_pos_a = abs(ma.get_tacho().rotation_count - pos1a)
+    new_pos_b = abs(mb.get_tacho().rotation_count - pos1b)
+    new_pos_c = abs(mc.get_tacho().rotation_count - pos1b)
+    power_a = -1*int(copysign(working_power,ma.get_tacho().rotation_count - pos1a))
+    power_b = -1*int(copysign(working_power,mb.get_tacho().rotation_count - pos1b))
+    power_c = -1*int(copysign(working_power,mc.get_tacho().rotation_count - pos1b))
+    runinstruction(PORT_A, power_a, new_pos_a);
+    runinstruction(PORT_B, power_b, new_pos_b);
+    runinstruction(PORT_C, power_c, new_pos_c);
     #    m.cmd(PORT_A, power, new_pos, 1,1,1)
     time.sleep(1)
-    print "After move 1: ", m1.get_tacho().rotation_count
+    print "After move 1: ", ma.get_tacho().rotation_count
     
-    new_pos = abs(m1.get_tacho().rotation_count - pos2)
-    print m1.get_tacho().rotation_count, pos2
-    power = -1*int(copysign(working_power,m1.get_tacho().rotation_count - pos2))
+    new_pos_a = abs(ma.get_tacho().rotation_count - pos2)
+    new_pos_b = abs(mb.get_tacho().rotation_count - pos2)
+    new_pos_c = abs(mc.get_tacho().rotation_count - pos2)
+    print ma.get_tacho().rotation_count, pos2
+    power_a = -1*int(copysign(working_power,ma.get_tacho().rotation_count - pos2))
+    power_b = -1*int(copysign(working_power,mb.get_tacho().rotation_count - pos2))
+    power_c = -1*int(copysign(working_power,mc.get_tacho().rotation_count - pos2))
     
     
-    print new_pos, power
-    runinstruction(PORT_A, power, new_pos);
-    runinstruction(PORT_B, power, new_pos);
-    runinstruction(PORT_C, power, new_pos);
+    print new_pos_a, power_a
+    runinstruction(PORT_A, power_a, new_pos_a);
+    runinstruction(PORT_B, power_b, new_pos_b);
+    runinstruction(PORT_C, power_c, new_pos_c);
     #    m.cmd(PORT_A, power, new_pos, 1,1,1)
     time.sleep(1)
-    print "After move 2: ", m1.get_tacho().rotation_count
+    print "After move 2: ", ma.get_tacho().rotation_count
 #
 #    time.sleep(1)
-#    print "loop: ", m1.get_tacho()
+#    print "loop: ", ma.get_tacho()
 #
 ##print m.is_ready(PORT_A)
 ##time.sleep(1)
